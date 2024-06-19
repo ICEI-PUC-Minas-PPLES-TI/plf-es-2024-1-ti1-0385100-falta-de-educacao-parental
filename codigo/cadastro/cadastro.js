d339a4b1e61d41b9a78df960391689a4eede6a91
Gabriela Alvarenga <160603188+gabialvarenga@users.noreply.github.com>
2024-06-18 21:30:05 -0300

    Alterações no login e cadastro

--- codigo/cadastro/cadastro.js
+++ codigo/cadastro/cadastro.js
@@ -1,68 +1,73 @@
 const apiUrl = 'https://d3bc0c71-a21a-467c-b5cc-9c4323769087-00-u725a4dkcvpy.worf.replit.dev/cadastro'; // URL da API JSONServer para usuários
 
-// Função para exibir mensagens na interface
-function displayMessage(message) {
-    const messageElement = document.getElementById('message');
-    messageElement.innerHTML = `<div class="alert alert-success">${message}</div>`;
+// Função para exibir mensagens na interface com estilo visível
+function displayMessage(message, type = 'success') {
+    const messageElement = document.getElementById('errorModalBody');
+
+    // Define a classe CSS com base no tipo de mensagem (success ou danger)
+    const alertType = type === 'success' ? 'alert-success' : 'alert-danger';
+
+    // Cria o elemento de mensagem com o estilo definido
+    const alertMessage = `<div class="alert ${alertType} error-box">${message}</div>`;
+
+    // Define o conteúdo HTML do elemento de mensagem
+    messageElement.innerHTML = alertMessage;
+
+    // Exibe o modal de erro
+    const modal = new bootstrap.Modal(document.getElementById('errorModal'));
+    modal.show();
 }
 
-// Função para verificar se o nome de usuário já está em uso
-function checkUsernameAvailability(login) {
-    return fetch(`${apiUrl}?login=${login}`)
+// Função para verificar se o email já está em uso
+function checkEmailAvailability(email) {
+    return fetch(`${apiUrl}?email=${email}`)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Erro ao verificar nome de usuário');
+                throw new Error('Erro ao verificar email');
             }
             return response.json();
         })
         .then(data => {
-            return data.length === 0; // Retorna verdadeiro se não houver nenhum usuário com o mesmo nome de usuário
+            return data.length === 0; // Retorna verdadeiro se não houver nenhum usuário com o mesmo email
         })
         .catch(error => {
-            console.error('Erro ao verificar nome de usuário via API JSONServer:', error);
+            console.error('Erro ao verificar email via API JSONServer:', error);
             return false; // Retorna falso em caso de erro
         });
 }
 
 // Função para adicionar um novo usuário
-function addUser(nome, login, senha, email) {
-    // Verifica a disponibilidade do nome de usuário antes de prosseguir
-    checkUsernameAvailability(login)
-        .then(isAvailable => {
-            if (!isAvailable) {
-                throw new Error('Nome de usuário já está em uso');
-            }
+function addUser(nome, login, senha, email, papel) {
+    const usuario = {
+        nome: nome,
+        login: login,
+        senha: senha,
+        email: email,
+        papel: papel
+    };
 
-            const usuario = {
-                nome: nome,
-                login: login,
-                senha: senha,
-                email: email
-            };
-
-            // Envia os dados do novo usuário para a API JSONServer via POST
-            return fetch(apiUrl, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(usuario),
-            });
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao cadastrar usuário');
-            }
-            return response.json();
-        })
-        .then(data => {
-            displayMessage("Usuário cadastrado com sucesso");
-            window.location.href = 'login.html'; // Redireciona para a página de login após o cadastro
-        })
-        .catch(error => {
-            console.error('Erro ao cadastrar usuário via API JSONServer:', error);
-            displayMessage("Erro ao cadastrar usuário");
-        });
+    // Envia os dados do novo usuário para a API JSONServer via POST
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(usuario),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao cadastrar usuário');
+        }
+        return response.json();
+    })
+    .then(data => {
+        displayMessage("Usuário cadastrado com sucesso");
+        window.location.href = 'login.html'; // Redireciona para a página de login após o cadastro
+    })
+    .catch(error => {
+        console.error('Erro ao cadastrar usuário via API JSONServer:', error);
+        displayMessage(error.message, 'danger');
+    });
 }
 
 // Função para lidar com o evento de submissão do formulário de cadastro
@@ -74,14 +79,15 @@ function handleRegister(event) {
     const email = document.getElementById('email').value;
     const confirmarEmail = document.getElementById('confirmar-email').value;
     const senha = document.getElementById('senha').value;
+    const papel = document.getElementById('papel').value;
 
     if (email !== confirmarEmail) {
-        displayMessage("Os campos de email não coincidem.");
+        displayMessage("Os campos de email não coincidem.", 'danger');
         return;
     }
 
     // Chama a função para adicionar o usuário com os dados fornecidos
-    addUser(nome, login, senha, email);
+    addUser(nome, login, senha, email, papel);
 }
 
 // Adiciona um ouvinte de eventos para o formulário de cadastro
